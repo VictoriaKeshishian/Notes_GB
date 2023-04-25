1a188d7cfdb2f1ba055594115a2447c46a75d2db
Victoria Keshishian <viky_lechka@mail.ru>
2023-04-25 16:16:16 +0300

    add main window

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from tkinter import *
 
+root = Tk()
+root.title('Заметки')
+root.geometry('600x700')
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+
+text = Text(root, width=600, height=700)
+text.pack()
+
+root.config(menu=menu_bar)
+root.mainloop()
